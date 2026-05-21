5f43f4a74694bdccdc390e7427e8a40bd29dac74
Gio-Padilla <pad24005@byui.edu>
2026-05-21 11:36:50 -0500

    Completed chapter 15

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,6 +4,7 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
  
 export default async function Page() {
   return (
@@ -27,3 +28,7 @@ export default async function Page() {
     </main>
   );
 }
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
